f9908071ec92861e24a27bbba60ae2fd658f6117
MargNation <margaret.w.harriman@gmail.com>
2017-01-08 20:23:38 -0500

    Updated from Codepen

--- Data Structures Viz/README.txt
+++ Data Structures Viz/README.txt
@@ -1,3 +1,5 @@
 A Pen created at CodePen.io. You can find this one at http://codepen.io/MargNation/pen/JEdyob.
 
- 
+This pen will create various data structures from sets of numbers and will allow the user to 
+step through each structure and learn how each is created. Hopefully CS students (and others!) 
+will benefit from this program.
--- Data Structures Viz/js/index.js
+++ Data Structures Viz/js/index.js
@@ -1,29 +1,31 @@
-window.requestAnimFrame = ( function() {
+window.requestAnimFrame = (function() {
 	return window.requestAnimationFrame ||
-				window.webkitRequestAnimationFrame ||
-				window.mozRequestAnimationFrame ||
-				function( callback ) {
-					window.setTimeout( callback, 1000 / 60 );
-				};
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		function(callback) {
+			window.setTimeout(callback, 1000 / 60);
+		};
 })();
 
-var canvas = document.getElementById('canvas'), 
-		ctx = canvas.getContext('2d'), 
-		canvasWidth = window.innerWidth, 
-		canvasHeight = window.innerHeight, 
-		numbers = [];
+var canvas = document.getElementById('canvas'),
+	ctx = canvas.getContext('2d'),
+	canvasWidth = window.innerWidth - 50,
+	canvasHeight = window.innerHeight + 200,
+	numbers = [];
 
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
+ctx.fillRect(0, 0, canvas.width, 7);
+
 // Simple random number generator
-function random( min, max ) {
-	return Math.random() * ( max - min ) + min;
+function random(min, max) {
+	return Math.random() * (max - min) + min;
 }
 
 // Generate random numbers to populate the structures
 function randomizer() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 7, canvas.width, canvas.height);
 	numbers = [];
 	for (var i = 0; i < 8; i++) {
 		numbers.push(Math.floor(random(0, 100)));
@@ -31,50 +33,76 @@ function randomizer() {
 	document.getElementById("numbersArea").innerHTML = numbers;
 }
 
-function createBinaryTree(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+function createBinaryTree() {
+	ctx.clearRect(0, 7, canvas.width, canvas.height);
 	// ctx.beginPath();
 	// ctx.arc(100,75,50,0,2*Math.PI);
 	// ctx.stroke();
-	ctx.font="24px Reem Kufi";
-	ctx.fillText("Insert Binary Tree here - " + numbers,50,50);
+	ctx.font = "24px Comfortaa";
+	ctx.fillText("Insert Binary Tree here - " + numbers, 50, 50);
 	return;
 }
 
-function createQueue(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// ctx.beginPath();
-	// ctx.arc(200,150,50,0,2*Math.PI);
-	// ctx.stroke();
-	ctx.font="24px Reem Kufi";
-	ctx.fillText("Insert Queue here - " + numbers,50,50);
+function createQueue() {
+	ctx.clearRect(0, 7, canvas.width, canvas.height);
+	var startingTopLeft = canvasWidth / 5.5;
+	var center = [startingTopLeft + 35, 160];
+	ctx.font="22px Comfortaa";
+	ctx.fillText("Index #:", center[0] - 125, center[1] + 75);
+	for (var i = 0; i < numbers.length; i++) {
+		ctx.strokeStyle = "#000000";
+		ctx.lineWidth = 7;
+		ctx.strokeRect(startingTopLeft, 100, 100, 100);
+		ctx.font="28px Comfortaa";
+		ctx.fillStyle = "white";
+		ctx.fillText(numbers[i], center[0], center[1]);
+		ctx.font="22px Comfortaa";
+		ctx.fillStyle = "black";
+		ctx.fillText(i, center[0], center[1] + 75);
+		startingTopLeft += 100;
+		center[0] += 100;
+	}
+}
+
+function createStack() {
+	ctx.clearRect(0, 7, canvas.width, canvas.height);
+	var startingX = (canvasWidth / 2) - 75;
+	var startingY = 75;
+	var center = [startingX + 35, startingY + 35];
+	ctx.font="22px Comfortaa";
+	ctx.fillText("Index #:", center[0] - 125, center[1] + 75);
+	for (var i = 0; i < numbers.length; i++) {
+		ctx.strokeStyle = "#000000";
+		ctx.lineWidth = 7;
+		ctx.strokeRect(startingX, startingY, 75, 75);
+		ctx.font="28px Comfortaa";
+		ctx.fillStyle = "white";
+		ctx.fillText(numbers[numbers.length - 1 - i], center[0], center[1]);
+		ctx.font="22px Comfortaa";
+		ctx.fillStyle = "black";
+		ctx.fillText((numbers.length - i), center[0], center[1] + 75);
+		startingY += 75;
+		center[0] += 75;
+	}
+}
+
+function createLinkedList() {
+	ctx.clearRect(0, 7, canvas.width, canvas.height);
+	ctx.font = "24px Comfortaa";
+	ctx.fillText("Insert Linked List here - " + numbers, 50, 50);
 	return;
 }
 
-function createStack(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.font="24px Reem Kufi";
-	ctx.fillText("Insert Stack here - " + numbers,50,50);
+function createHeap() {
+	ctx.clearRect(0, 7, canvas.width, canvas.height);
+	ctx.font = "24px Comfortaa";
+	ctx.fillText("Insert Heap here - " + numbers, 50, 50);
 	return;
 }
 
-function createLinkedList(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.font="24px Reem Kufi";
-	ctx.fillText("Insert Linked List here - " + numbers,50,50);
-	return;
-}
-
-function createHeap(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.font="24px Reem Kufi";
-	ctx.fillText("Insert Heap here - " + numbers,50,50);
-	return;
-}
-
-function createDblLinkedList(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.font="24px Reem Kufi";
-	ctx.fillText("Insert Doubly Linked List here - " + numbers,50,50);
+function createDblLinkedList() {
+	ctx.clearRect(0, 7, canvas.width, canvas.height);
+	ctx.font = "24px Comfortaa";
+	ctx.fillText("Insert Doubly Linked List here - " + numbers, 50, 50);
 	return;
 }
